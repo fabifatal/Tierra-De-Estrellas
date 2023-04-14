f27fc78995937e69e901da4e88e14499ec6fe2c0
fabifatal <fabian.zuniga.p@gmail.com>
2023-04-14 00:07:00 -0400

    juego draggeable

--- React/tdar/src/components/Barra Inferior/MapaUniverso.js
+++ React/tdar/src/components/Barra Inferior/MapaUniverso.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./MapaUniverso.css";
 import mapa from "../../imagenes/img-juego/mapa1.jpg";
+import mapagrande from "../../imagenes/img-juego/eso0116a_El campo profundo Capodimonte.jpg"
+import { useDraggable } from "react-use-draggable-scroll";
 
-const MapaNocturno = () => {
-  return (
-    <div className="contMapa">
-      <div className="contContaminacion tamanioMapa">
-        <div className="contaminacionlum"></div>
-      </div>
-      <div className="contImgMapa tamanioMapa">
-        <img className="bg-img imgMapa" src={mapa} />
-      </div>
-    </div>
-  );
-};
+
+
+const MapaNocturno = () =>{
+  const ref = useRef();
+  const { events} = useDraggable(ref);
+  return (<div className="contImgMapa"{...events} ref={ref}>
+    <img className="imgMapa"src={mapagrande} alt="Campo Profundo Capodimonte"/>
+ 
+  </div>
+
+  )
+
+}
+
+// const MapaNocturno = () => {
+//   return (
+//     <div className="contMapa">
+//       <div className="contContaminacion tamanioMapa">
+//         <div className="contaminacionlum"></div>
+//       </div>
+//       <div className="contImgMapa tamanioMapa">
+//         <img className="bg-img imgMapa" src={mapa} />
+//       </div>
+//     </div>
+//   );
+// };
 
 export default MapaNocturno;
